05e30810eda5b3b78a57ee06d9fe8cca798d3d94
BuildTools <unconfigured@null.spigotmc.org>
2024-05-15 23:40:34 +0200

    d

--- scripts.js
+++ scripts.js
@@ -1,7 +1,32 @@
+
+
 const copyIPBtn = document.getElementById("copyIPBtn");
 
-copyIPBtn.addEventListener("click", function() {
-    const ipAddress = "192.168.1.10"; // Ersetze dies durch deine Server-IP
-    navigator.clipboard.writeText(ipAddress);
-    alert("IP-Adresse in die Zwischenablage kopiert!");
-});
+//html <button onclick="copyIP()">Play Now</button>
+
+
+
+
+function copyIP() {
+    const ip = "yellowmc.de";
+    navigator.clipboard.writeText(ip);
+    const copiedLinkBox = document.querySelector(".copiedlinkbox");
+    copiedLinkBox.style.display = "block";
+    copiedLinkBox.addEventListener('animationend', () => {
+        copiedLinkBox.style.display = 'none';
+    });
+}
+
+//css in styles.css
+// .copiedlinkbox {
+//     display: none;
+//     position: fixed;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     background-color: #333;
+//     color: #fff;
+//     padding: 10px 0;
+//     text-align: center;
+//     z-index: 1000;
+// }
